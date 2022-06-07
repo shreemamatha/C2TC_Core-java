72dad01951b640edb721218222882a8988a88b7b
Mamatha <Mamatha@DORAEMON>
2022-06-07 16:16:08 +0530

    first commit

---
+++ acharya_6a/src/acharya_6a/float_1.java
@@ -0,0 +1,20 @@
+package acharya_6a;
+
+public class float_1 {
+
+	public static void main(String[] args) {
+		/*
+		int value1=9/2;
+		float value2=10f/6f
+		double value3=10d/6d;
+		system.out.println("value 1 ="+value1);
+		system.out.println("value 2 ="+value2);
+		system.out.println("value 3 ="+value3);
+		*/
+		int marker = 512;
+		double percentage  = marker*0.46f;
+		
+		System.out.println("percentage:"+percentage);
+	}
+
+}

3b3675597ad2dd5913e4ce20ef8d9a98a7057cf9
Mamatha <Mamatha@DORAEMON>
2022-06-07 16:52:00 +0530

    first commit

---
+++ acharya_6g/src/acharya_6g/hello8.java
@@ -0,0 +1,15 @@
+package acharya_6g;
+
+public class hello8 {
+
+	public static void main(String[] args) {
+		int x=5;
+		if(x!=5);
+		{
+			System.out.println("value of x is not 5");
+		}
+		{
+		System.out.println("value of x is 5");
+		}
+	}
+}

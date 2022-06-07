0a3f32ccb8ba2339851454ea84c2ad12e327c1f0
Mamatha <Mamatha@DORAEMON>
2022-06-07 16:43:46 +0530

    first commit

---
+++ acharya_6f/src/acharya_6f/hello7.java
@@ -0,0 +1,12 @@
+package acharya_6f;
+
+public class hello7 {
+
+	public static void main(String[] args) {
+		int x=10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+	}
+
+}

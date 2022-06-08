a9f6da9c320d39424135a8d8c059fbd98dbac91b
Mamatha <Mamatha@DORAEMON>
2022-06-08 08:36:20 +0530

    first commit

---
+++ acharya_6k/src/acharya_6k/hello12.java
@@ -0,0 +1,16 @@
+package acharya_6k;
+
+public class hello12 {
+
+	public static void main(String[] args) {
+		int x = 4;
+		//x+=5;//x=x+5
+		//x-=5;//x=x-5
+		//x*=5;//x=x*5
+		//x/=4;//x=x/4
+		x%=5;//x=x%5
+		System.out.println(x);
+
+	}
+
+}
